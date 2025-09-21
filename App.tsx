28c8b4f9133d42f4d4ec999c48097a58bf438a53
Hiromichi5 <sc23557z@st.omu.ac.jp>
2025-09-21 12:09:06 +0900

    2画面追加

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SecondScreen from './SecondScreen';
+
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }: any) {
   return (
     <View style={styles.container}>
       <Text>ハローワルド</Text>
+      <Button title="次の画面へ" onPress={() => navigation.navigate('Second')} />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'ホーム' }} />
+        <Stack.Screen name="Second" component={SecondScreen} options={{ title: '2画面目' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
